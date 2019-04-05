9f908fbb40d32e281bab42bc24a03ae15850ed0f
sambey04 <samarthh44@gmail.com>
2019-04-05 16:18:11 +0530

    new

--- Main.c
+++ Main.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-
-int main()
+#include<stdlib.h>
+void main()
 {
-	printf("hello world");
-}
+int r,n;
+   time_t t;
+	   srand((unsigned) time(&t));
+  for(n=0;n<=100;n++){ 
+
+
+	   printf("time is=%ld \n",time(&t));
+//delay(1000);
+	   r = rand();
+printf("%d",r % (122 + 1-97)+97);
+}}
