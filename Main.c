63bb95da18856a8ce5f2cdcaab9c155e1e3b85df
sambey04 <samarthh44@gmail.com>
2019-08-09 11:39:42 +0530

    printing_words

--- Main.c
+++ Main.c
@@ -1,15 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+void scanfunc();
+
+
 void main()
 {
-int r,n;
+	
+char  str2[5];
+	char str1[5];
+int r,inner_n,outer_n;
    time_t t;
-	   srand((unsigned) time(&t));
-  for(n=0;n<=100;n++){ 
+int array_word[5];
+   srand((unsigned) time(&t));
+
+   for(outer_n=0;outer_n>=0;outer_n++){
+       for(inner_n=0;inner_n<=5;inner_n++){ 
 
 
-	   printf("time is=%ld \n",time(&t));
-//delay(1000);
-	   r = rand();
-printf("%d",r % (122 + 1-97)+97);
+	  // printf("        time is=%ld \n",time(&t));
+
+	
+		   r = rand();
+array_word[inner_n]=("%c \n",r % (122 + 1-97)+97);
+printf("%c ",array_word[inner_n]);
+	 }
+  scanfunc();
+   
 }}
+
+
+
+void scanfunc()
+{
+
+     
+char str1[5];
+	scanf("%s", str1);
+
+}
