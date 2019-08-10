b3252d3134ab2344c7e5587fbf231d0ebcb4890d
Noobcoder777 <mishtashashank>
2019-08-10 15:39:11 +0530

    added new changes

---
+++ TT1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "TT2.h"
+#define MAX_WORD   100
+#define NUM_WORDS  10
+   
+int main(int argc, char const *argv[])
+{
+    
+    return func1();
+}
+
+
---
+++ TT2.h
@@ -0,0 +1,17 @@
+int func1(void)
+{
+    /* Open words file */
+    FILE* fp = NULL;
+    char words[20];
+    int i = 0 , ran = 0;
+    srand(time(NULL));
+    fp = fopen("filett.txt" , "r+");
+    for(; fgets(words , sizeof(words) , fp) ; i++)
+          ;
+        ran = rand() % i;
+    rewind(fp);
+    for(i = 0 ; i < ran ; i++)
+        fgets(words , sizeof(words) , fp);
+        printf("%s" , words);
+    return 0;
+}
