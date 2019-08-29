a2e24b6e1be0c9a2104a92ad5fae3278046fbd73
ram <ram@localhost.localdomain>
2019-08-29 04:35:32 -0500

    new_files_loaded

---
+++ save_word.h
@@ -0,0 +1,12 @@
+void enter_data()
+{
+	//this function is for entering data in a file
+ char str[20];
+ FILE *fptr;
+ fptr = fopen("user12.txt","w");
+ printf("Enter input: ");
+ scanf("%[^\n]%*c", str);  
+ fprintf(fptr,"%s",str);
+ fclose(fptr);
+ return 0;
+ }
